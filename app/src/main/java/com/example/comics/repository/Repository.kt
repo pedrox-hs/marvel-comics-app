af22756ad56f94033551f16a0551253b438730cc
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-08-17 16:58:26 -0300

    Prepare project for instrumented tests
    - Enable build config feature
    - Use build config field to provide API base URL
    - Add required test dependencies (Espresso Contrib, Runner, Rules and MockWebServer)
    - Add OkHttp dependencies required by MockWebServer
    - Setup Espresso
    - Make gradlew file executable

--- app/src/main/java/com/example/comics/repository/Repository.kt
+++ app/src/main/java/com/example/comics/repository/Repository.kt
@@ -1,18 +1,23 @@
 package com.example.comics.repository
 
+import com.example.comics.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.await
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class Repository {
 
     suspend fun getComics() = Retrofit.Builder()
-        .baseUrl("https://gateway.marvel.com/v1/public/")
+        .baseUrl(BuildConfig.API_BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .build().create(Api::class.java).getComics(
+        .build()
+        .create<Api>()
+        .getComics(
             apikey = "b7e14bab409c70a5c4e7c2b319c09d7b",
             ts = "1682982412",
             hash = "3482f01e9bf207a437a4b621c91339ad"
-        ).await()
+        )
+        .await()
 
 }
