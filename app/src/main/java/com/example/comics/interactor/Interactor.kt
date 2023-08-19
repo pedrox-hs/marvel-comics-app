de0e80eace26bfef9540acc4aaf464b1621a24bf
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-08-18 22:14:59 -0300

    Remove unnecessary safeRunDispatcher

--- app/src/main/java/com/example/comics/interactor/Interactor.kt
+++ app/src/main/java/com/example/comics/interactor/Interactor.kt
@@ -2,9 +2,6 @@ package com.example.comics.interactor
 
 import com.example.comics.domain.repository.ComicsRepository
 import com.example.comics.presenter.IPresenter
-import com.example.comics.util.Result.Failure
-import com.example.comics.util.Result.Success
-import com.example.comics.util.safeRunDispatcher
 import org.koin.core.annotation.Factory
 
 @Factory
@@ -12,15 +9,13 @@ class Interactor(
     private val iPresenter: IPresenter,
     private val repository: ComicsRepository,
 ) : IInteractor {
-
-
     override suspend fun getComics() {
-        when (val result = safeRunDispatcher {
-            repository.fetch()
-        }) {
-            is Success -> iPresenter.setupList(result.data)
-            is Failure -> iPresenter.error()
-        }
+        runCatching { repository.fetch() }
+            .onSuccess {
+                iPresenter.setupList(it)
+            }
+            .onFailure {
+                iPresenter.error()
+            }
     }
-
 }
